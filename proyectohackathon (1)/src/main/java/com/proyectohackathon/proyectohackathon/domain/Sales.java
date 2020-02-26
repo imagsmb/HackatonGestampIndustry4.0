f6ef1e2748a457673d31ad6e1d531e69ea7b55c6
Pavel Goikoetxea Mallea <pavelgoikoetxea@gmail.com>
2020-02-26 20:20:12 +0100

    Modificado Domain
    se ha añadido @Field encima de cada campo

--- proyectohackathon (1)/src/main/java/com/proyectohackathon/proyectohackathon/domain/Sales.java
+++ proyectohackathon (1)/src/main/java/com/proyectohackathon/proyectohackathon/domain/Sales.java
@@ -16,23 +16,51 @@ import javax.validation.constraints.NotNull;
 @Document(collection = "sales")
 public class Sales {
     @Id
-    @Field
+    @Field("_id")
     private String id;
     @NotNull
     @NotBlank
+
+    @Field("Region")
     private String region;
+
+    @Field("Country")
     private String country;
+
+    @Field("Item Type")
     private String item_type;
+
+    @Field("Sales Channel")
     private String sales_channel;
+
+    @Field("Order Priority")
     private String order_priority;
+
+    @Field("Order Date")
     private String order_date;
+
+    @Field("Order ID")
     private String order_id;
+
+    @Field("Ship Date")
     private String ship_date;
+
+    @Field("Units Sold")
     private String units_sold;
+
+    @Field("Unit Price")
     private String unit_price;
+
+    @Field("Unit Cost")
     private String unit_cost;
+
+    @Field("Total Revenue")
     private String total_revenue;
+
+    @Field("Total Cost")
     private String total_cost;
+
+    @Field("Total Profit")
     private String total_profit;
 
 }
